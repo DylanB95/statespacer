a07eaaaf4798e4a04866bf4c8eee9e34f44ac3da
DylanB95 <dylanbeijers@gmail.com>
2020-04-29 12:38:53 +0200

    Making utilities available

--- R/AnsleyKohn.R
+++ R/AnsleyKohn.R
@@ -1,52 +1,52 @@
 #' Transform arbitrary matrices into partial autocorrelation matrices
-#' 
+#'
 #' Creates valid partial autocorrelation matrices.
-#' 
+#'
 #' @param A An array of arbitrary square matrices in the multivariate case,
 #'   or a vector of arbitrary numbers in the univariate case.
-#' 
+#'
 #' @return An array of partial autocorrelation matrices in the multivariate case,
 #'   or a vector of partial autocorrelations in the univariate case.
-#' 
+#'
 #' @noRd
 PACMat <- function(A) {
-  
+
   # If univariate, computations are less expensive
   if (is.vector(A)) {
     return(A / sqrt(1 + A^2))
   }
-  
+
   # Multivariate, for a single partial autocorrelation matrix
   if (is.matrix(A)) {
     return(solve(t(chol(diag(dim(A)[1]) + A %*% t(A)))) %*% A)
   }
-  
+
   # Multivariate, for multiple partial autocorrelation matrices
   return(array(apply(A, 3, PACMat), dim = dim(A)))
 }
 
 #' Transform partial autocorrelation matrices into coefficient matrices
-#' 
+#'
 #' Creates coefficient matrices for which the characteristic polynomial
-#' corresponds to a stationary process. Note, this function covers a 
-#' subset of the space of coefficient matrices that correspond to a 
+#' corresponds to a stationary process. Note, this function covers a
+#' subset of the space of coefficient matrices that correspond to a
 #' stationary process.
-#' 
+#'
 #' @param P An array of partial autocorrelation matrices in the multivariate case,
 #'   or a vector of partial autocorrelations in the univariate case.
-#' 
+#'
 #' @return
 #' Multivariate case:
 #'   A list containing:
 #'     An array of coefficient matrices.
-#'     Variance - covariance matrix of the noise, in order to transform 
+#'     Variance - covariance matrix of the noise, in order to transform
 #'       coefficients further.
 #' Univariate case:
 #'   A vector of coefficients.
-#' 
+#'
 #' @noRd
 TransformPAC <- function(P) {
-  
+
   coeff_new <- P
 
   # If univariate, computations are less expensive
@@ -61,28 +61,28 @@ TransformPAC <- function(P) {
     }
     return(coeff_new)
   }
-  
+
   # Multivariate
   # Initialise with i = 0
   sigma_new <- diag(dim(P)[1]) - coeff_new[,,1] %*% t(coeff_new[,,1])
-  
+
   if (dim(P)[3] > 1) {
-    
+
     # i = 0
     coeff_star_new <- P
     coeff_star_new[,,1] <- t(P[,,1])
     sigma_star_new <- diag(dim(P)[1]) - coeff_star_new[,,1] %*% t(coeff_star_new[,,1])
     L <- t(chol(sigma_new))
     L_star <- t(chol(sigma_star_new))
-    
+
     for (i in 1:(dim(P)[3] - 1)) {
-  
+
       # Storing former values
       coeff_old <- coeff_new
       coeff_star_old <- coeff_star_new
       sigma_old <- sigma_new
       sigma_star_old <- sigma_star_new
-      
+
       # Calculating new values
       coeff_new[,,i + 1] <- L %*% P[,,i + 1] %*% solve(L_star)
       sigma_new <- sigma_old - coeff_new[,,i + 1] %*% sigma_star_old %*% t(coeff_new[,,i + 1])
@@ -104,26 +104,39 @@ TransformPAC <- function(P) {
 }
 
 #' Transform arbitrary matrices into ARMA coefficient matrices
-#' 
+#'
 #' Creates coefficient matrices for which the characteristic polynomial
 #' corresponds to a stationary process.
-#' 
+#' See \insertCite{ansley1986note;textual}{statespacer} for details about
+#' the transformation used.
+#'
 #' @param A An array of arbitrary square matrices in the multivariate case,
 #'   or a vector of arbitrary numbers in the univariate case.
-#' @param variance A variance - covariance matrix in the multivariate case,
-#'   or the variance in the univariate case.
-#' 
-#' @return A list containing:
-#'   Multivariate case:
-#'     An array of coefficient matrices for the AR part.
-#'     An array of coefficient matrices for the MA part.
-#'   Univariate case:
-#'     A vector of coefficients for the AR part.
-#'     A vector of coefficients for the MA part.
-#' 
-#' @noRd
+#' @param variance A variance - covariance matrix.
+#'   Note: `variance` not needed for the univariate case!
+#' @param ar The order of the AR part.
+#' @param ma The order of the MA part.
+#'
+#' @return
+#' If multivariate, a list containing:
+#' * An array of coefficient matrices for the AR part.
+#' * An array of coefficient matrices for the MA part.
+#'
+#' If univariate, a list containing:
+#'   * A vector of coefficients for the AR part.
+#'   * A vector of coefficients for the MA part.
+#'
+#' @author Dylan Beijers, \email{dylanbeijers@@gmail.com}
+#'
+#' @references
+#' \insertRef{ansley1986note}{statespacer}
+#'
+#' @examples
+#' CoeffARMA(A = stats::rnorm(2), ar = 1, ma = 1)
+#'
+#' @export
 CoeffARMA <- function(A, variance = NULL, ar = 1, ma = 0) {
-  
+
   # Check for erroneous input
   if (ar < 0) {
     stop("The order of the autoregressive part must be >= 0.")
@@ -134,13 +147,13 @@ CoeffARMA <- function(A, variance = NULL, ar = 1, ma = 0) {
   if (ar + ma == 0) {
     stop("At least one of the orders of the AR and MA parts must be positive.")
   }
-  
+
   # Initialise list to return
   result <- list()
-  
+
   # Obtain partial autocorrelation matrices
   P <- PACMat(A)
-  
+
   # If univariate, coefficients are readily returned by TransformPAC
   if (is.vector(P)) {
     if (ar > 0) {
@@ -153,11 +166,11 @@ CoeffARMA <- function(A, variance = NULL, ar = 1, ma = 0) {
     }
     return(result)
   }
-  
+
   # Cholesky decomposition of variance - covariance matrix
   L <- t(chol(variance))
   L_inv <- solve(L)
-  
+
   # Obtain coefficient matrices for AR part
   if (ar > 0) {
     P_ar <- P[,,1:ar, drop = FALSE]
@@ -169,7 +182,7 @@ CoeffARMA <- function(A, variance = NULL, ar = 1, ma = 0) {
       dim = dim(ar_part$coeff)
     )
   }
-  
+
   # Obtain coefficient matrices for MA part
   if (ma > 0) {
     P_ma <- P[,,(ar + 1):(ar + ma), drop = FALSE]
