798e283a99725eedca2c01bc99a41862ecc4a60c
DylanB95 <dylanbeijers@gmail.com>
2023-01-27 22:22:35 +0100

    Updates RcppExports file

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // ExtractComponentC
 arma::cube ExtractComponentC(const arma::cube& a, const arma::cube& Z);
 RcppExport SEXP _statespacer_ExtractComponentC(SEXP aSEXP, SEXP ZSEXP) {
